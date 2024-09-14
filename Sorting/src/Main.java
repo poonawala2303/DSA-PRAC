1da2227e5a2b514597c3769fa55dc39d597a49ce
poonawala2303 <mustafa.poonawala82@gmail.com>
2024-09-14 09:49:18 +0530

    Done till Cyclic Sort

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -4,9 +4,23 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
 //        bubbleSort(arr);
-        selectionSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
      }
+
+    static void insertionSort(int []arr){
+        for (int i = 0; i <= arr.length -2; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
 
     static void selectionSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
---
+++ Sorting/src/SetMismatch.java
@@ -0,0 +1,35 @@
+public class SetMismatch {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findErrorNums(int[] arr) {
+        int i=0;
+
+        while(i< arr.length){
+            int correct = arr[i] - 1;
+            if( /*arr[i] < arr.length && */ arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+
+        //srch for missing number
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index]!= index+1){
+                return new int[]{arr[index],index+1};
+            }
+        }
+
+        // case 2
+        return new int[]{-1,-1};
+    }
+
+    void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
